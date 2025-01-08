af1ee28c9c56ebb7d1bc52ba678c3268c60623d9
Issa Oulkhir <aissaoulkhir22@gmail.com>
2025-01-08 11:32:16 +0100

    more clean

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 19:09:02 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/01/08 10:56:04 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/01/08 11:31:51 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define ALGORITHM_H
 
 # include "../push_swap.h"
-#include "../stack/stack.h"
+# include "../stack/stack.h"
 
-#define INVALID_IDX -314
+# define INVALID_IDX -314
 
 /*
 ALGO :
@@ -26,11 +26,10 @@ ALGO :
 	FIND the min						ra, rra
 */
 
-#define ABS_VAL(x) ((x > 0) - 0.5) * 2 * (x)
-
 void	my_algo(t_parsed_data *my_data, t_mystack **a_b[2]);
 
-void	mov_to_top(t_parsed_data *my_data, t_mystack **a_b[2], int index, int data);
+void	mov_to_top(t_parsed_data *my_data, t_mystack **a_b[2],
+		int index, int data);
 void	find_LIS_and_push(t_parsed_data *my_data, t_mystack **a_b[2]);
 void	calc_rotate_push(t_parsed_data *my_data, t_mystack **a_b[2]);
 
